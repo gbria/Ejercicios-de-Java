d25dcf1cc37c74f5001fa4ac7d4ece63e82fd3a2
Germán Bria Mayorga <germanbria@gmail.com>
2022-10-14 11:23:12 +0200

    Figuras

--- DAM1/IterativeExercices/src/main/java/iterativeExercices/Exercici13.java
+++ DAM1/IterativeExercices/src/main/java/iterativeExercices/Exercici13.java
@@ -11,15 +11,15 @@ import java.util.Scanner;
 public class Exercici13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int opcion;
+        int opcion, colFinal, colIni;
 
-        System.out.println("Introduzca una opción");
-        System.out.println("1. Opción");
-        System.out.println("2. Opción");
-        System.out.println("3. Opción");
-        System.out.println("4. Opción");
-        System.out.println("5. Opción");
-        System.out.println("6. Opción");
+        System.out.println("Menú figuras");
+        System.out.println("1. Opción: Cuadrado");
+        System.out.println("2. Opción: Triangulo");
+        System.out.println("3. Opción: Triangulo Distorsionado");
+        System.out.println("4. Opción: Pirámide");
+        System.out.println("5. Opción: Pirámide numérica");
+        System.out.println("6. Opción: Pirámide numérica mejorado");
         System.out.print("Opción: ");
         opcion = sc.nextInt();
 
@@ -33,6 +33,7 @@ public class Exercici13 {
                     System.out.println();
                 }
                 break;
+
             case 2:
                 for (int f = 0; f < 5; f++) {
                     for (int c = -1; c < f; c++) {
@@ -41,9 +42,10 @@ public class Exercici13 {
                     System.out.println("");
                 }
                 break;
+
             case 3:
-                int colFinal = 8;
-                int colIni = 8;
+                colFinal = 8;
+                colIni = 8;
                 for (int f = 0; f < 5; f++) {
                     //System.out.print("Fila: " + f + " ");
                     for (int c = 0; c < 9; c++) {
@@ -62,18 +64,71 @@ public class Exercici13 {
                 break;
 
             case 4:
+                colFinal = 4;
+                colIni = 4;
+                for (int f = 0; f < 5; f++) {
+                    //System.out.print("Fila: " + f + " ");
+                    for (int c = 0; c < 9; c++) {
+                        if (c < colIni) {
+                            System.out.print("  ");
+                        } else if (c <= colFinal) {
+                            System.out.print("* ");
+                        } else {
+                            System.out.print("  ");
+                        }
+                    }
+                    colFinal++;
+                    colIni--;
+                    System.out.println();
+                }
                 break;
+
             case 5:
+                colFinal = 4;
+                colIni = 4;
+                for (int f = 0; f < 5; f++) {
+                    //System.out.print("Fila: " + f + " ");
+                    for (int c = 0; c < 9; c++) {
+                        if (c < colIni) {
+                            System.out.print("  ");
+                        } else if (c <= colFinal) {
+                            System.out.print((f + 1) + " ");
+                        } else {
+                            System.out.print("  ");
+                        }
+                    }
+                    colFinal++;
+                    colIni--;
+                    System.out.println();
+                }
                 break;
+
             case 6:
+                colFinal = 4;
+                colIni = 4;
+                for (int f = 0; f < 5; f++) {
+                    //System.out.print("Fila: " + f + " ");
+                    for (int c = 0, tmpIni = 5, tmpFinal = -3; c < 9; c++, tmpIni--, tmpFinal++) {
+                        if (c < colIni) {
+                            System.out.print("  ");
+                        } else if (c <= 4) {
+                            System.out.print(tmpIni + " ");
+                        } else if (c <= colFinal) {
+                            System.out.print(tmpFinal + " ");
+                        } else {
+                            System.out.print("  ");
+                        }
+                    }
+                    colFinal++;
+                    colIni--;
+                    System.out.println();
+                }
                 break;
+
             default:
                 System.out.println("Error. La opción no existe");
                 break;
 
-
         }
-
-
     }
 }
