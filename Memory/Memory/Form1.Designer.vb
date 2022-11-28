ae577b59d036abb4249f6cb9ec540a41492d940d
Germán Bria Mayorga <germanbria@gmail.com>
2022-11-28 18:04:14 +0100

    Memory

---
+++ Memory/Memory/Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.LblFail = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.BtnRestart = New System.Windows.Forms.Button()
+        Me.LblTime = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.LblLevel = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LblScore = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.AyudaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AcercaDeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'LblFail
+        '
+        Me.LblFail.AutoSize = True
+        Me.LblFail.Font = New System.Drawing.Font("Noto Sans", 21.75!, System.Drawing.FontStyle.Bold)
+        Me.LblFail.Location = New System.Drawing.Point(955, 324)
+        Me.LblFail.Name = "LblFail"
+        Me.LblFail.Size = New System.Drawing.Size(34, 39)
+        Me.LblFail.TabIndex = 30
+        Me.LblFail.Text = "0"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Noto Sans", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.Label7.Location = New System.Drawing.Point(939, 276)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(66, 26)
+        Me.Label7.TabIndex = 29
+        Me.Label7.Text = "Fallos"
+        '
+        'BtnRestart
+        '
+        Me.BtnRestart.AutoSize = True
+        Me.BtnRestart.Font = New System.Drawing.Font("Noto Sans", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.BtnRestart.Location = New System.Drawing.Point(927, 517)
+        Me.BtnRestart.Name = "BtnRestart"
+        Me.BtnRestart.Size = New System.Drawing.Size(91, 32)
+        Me.BtnRestart.TabIndex = 28
+        Me.BtnRestart.Text = "Reiniciar"
+        Me.BtnRestart.UseVisualStyleBackColor = True
+        '
+        'LblTime
+        '
+        Me.LblTime.AutoSize = True
+        Me.LblTime.Font = New System.Drawing.Font("Noto Sans", 21.75!, System.Drawing.FontStyle.Bold)
+        Me.LblTime.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.LblTime.Location = New System.Drawing.Point(947, 433)
+        Me.LblTime.Name = "LblTime"
+        Me.LblTime.Size = New System.Drawing.Size(51, 39)
+        Me.LblTime.TabIndex = 25
+        Me.LblTime.Text = "30"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Noto Sans", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.Label6.Location = New System.Drawing.Point(931, 385)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(83, 26)
+        Me.Label6.TabIndex = 24
+        Me.Label6.Text = "Tiempo"
+        '
+        'LblLevel
+        '
+        Me.LblLevel.AutoSize = True
+        Me.LblLevel.Font = New System.Drawing.Font("Noto Sans", 21.75!, System.Drawing.FontStyle.Bold)
+        Me.LblLevel.Location = New System.Drawing.Point(955, 215)
+        Me.LblLevel.Name = "LblLevel"
+        Me.LblLevel.Size = New System.Drawing.Size(34, 39)
+        Me.LblLevel.TabIndex = 27
+        Me.LblLevel.Text = "1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Noto Sans", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.Label2.Location = New System.Drawing.Point(942, 167)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 26)
+        Me.Label2.TabIndex = 26
+        Me.Label2.Text = "Nivel"
+        '
+        'LblScore
+        '
+        Me.LblScore.AutoSize = True
+        Me.LblScore.Font = New System.Drawing.Font("Noto Sans", 21.75!, System.Drawing.FontStyle.Bold)
+        Me.LblScore.Location = New System.Drawing.Point(955, 106)
+        Me.LblScore.Name = "LblScore"
+        Me.LblScore.Size = New System.Drawing.Size(34, 39)
+        Me.LblScore.TabIndex = 23
+        Me.LblScore.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Noto Sans", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.Label3.Location = New System.Drawing.Point(913, 58)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(119, 26)
+        Me.Label3.TabIndex = 22
+        Me.Label3.Text = "Puntuación"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AyudaToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.MenuStrip1.Size = New System.Drawing.Size(1044, 24)
+        Me.MenuStrip1.TabIndex = 31
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'AyudaToolStripMenuItem
+        '
+        Me.AyudaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AcercaDeToolStripMenuItem})
+        Me.AyudaToolStripMenuItem.Name = "AyudaToolStripMenuItem"
+        Me.AyudaToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
+        Me.AyudaToolStripMenuItem.Text = "Ayuda"
+        '
+        'AcercaDeToolStripMenuItem
+        '
+        Me.AcercaDeToolStripMenuItem.Name = "AcercaDeToolStripMenuItem"
+        Me.AcercaDeToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AcercaDeToolStripMenuItem.Text = "Acerca de"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1044, 761)
+        Me.Controls.Add(Me.LblFail)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.BtnRestart)
+        Me.Controls.Add(Me.LblTime)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.LblLevel)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.LblScore)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LblFail As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents BtnRestart As Button
+    Friend WithEvents LblTime As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents LblLevel As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LblScore As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents AyudaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AcercaDeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Memory/Memory/Form1.vb
@@ -0,0 +1,268 @@
+﻿Imports System.Threading
+
+Public Class Form1
+    'LIST DE PICTURES Y NUMEROS
+    Dim pictures As List(Of PictureBox) = New List(Of PictureBox)
+    Dim numbers As New List(Of Integer)
+
+    'VARIABLES IMAGENES 1 Y 2
+    Private pictureA As PictureBox
+    Private pictureB As PictureBox
+
+    'VARIABLES OPCIONES
+    Private firstChoice As String
+    Private secondChoice As String
+
+    'VARIABLES DE TIEMPO
+    Private totalTime = 30
+    Private countDown As Integer
+
+    'OTRAS VARIABLES
+    Private numLevel As Integer = 1
+    Private qtyCards As Integer
+    Private score As Integer
+    Private tries As Integer
+    Private checkGameOver = False
+
+    Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckLevel(numLevel)
+        LoadPictures()
+    End Sub
+
+    Private Sub TimerEvent(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'RESTA EL TIEMPO
+        countDown -= 1
+        LblTime.Text = countDown
+
+        'GIRA LAS CARTAS
+        If countDown = 28 Then
+            BackCards()
+        End If
+
+        'SE ACABA EL TIEMPO
+        If countDown < 1 Then
+            GameOver("Has perdido. ")
+        End If
+
+    End Sub
+
+    Private Sub RestartGameEvent(sender As Object, e As EventArgs) Handles BtnRestart.Click
+        RestartGame()
+    End Sub
+
+    Private Sub LoadPictures()
+
+        'GENERA LOS NUMEROS (1,1,2,2,3,3...)
+        Dim a As Integer = 1
+        Dim b As Integer = 1
+        For index = 1 To qtyCards
+            numbers.Add(a)
+            numbers.Add(b)
+            a += 1
+            b += 1
+        Next
+
+        'POSICION DE LAS CARTAS
+        Dim leftPos As Integer = 20
+        Dim topPos As Integer = 40
+        Dim rows As Integer = 0
+
+        'COLOCACIÓN DE LAS CARTAS
+        For index = 1 To numbers.Count
+            Dim newPic As PictureBox = New PictureBox
+            newPic.Height = 250
+            newPic.Width = 200
+            newPic.Image = Image.FromFile("cards/back.png")
+            newPic.SizeMode = PictureBoxSizeMode.StretchImage
+
+            'EL EVENTO CLICK (newPic.Click) LLAMA A LA FUNCIÓN (PictureBox_Click) 
+            AddHandler newPic.Click, AddressOf PictureBox_Click
+            pictures.Add(newPic)
+
+            If rows < 4 Then
+                rows += 1
+                newPic.Left = leftPos
+                newPic.Top = topPos
+                Me.Controls.Add(newPic)
+                leftPos += 200
+            End If
+
+            If rows = 4 Then
+                leftPos = 20
+                topPos += 250
+                rows = 0
+            End If
+        Next
+
+        If numLevel > 1 Then
+            Shuffle()
+            Timer1.Start()
+            countDown = totalTime
+        Else
+            RestartGame()
+        End If
+
+    End Sub
+
+    Private Sub PictureBox_Click(picture As PictureBox, e As EventArgs)
+        'MOSTRAMOS SOLAMENTE DOS CARTAS MEDIANTE EL TAG
+
+        'PRIMERA OPCIÓN
+        If firstChoice Is Nothing Then
+            pictureA = picture
+            If pictureA.Tag IsNot Nothing Then
+                pictureA.Image = Image.FromFile("cards/" & CStr(pictureA.Tag) & ".png")
+                firstChoice = CStr(pictureA.Tag)
+            End If
+
+            'SEGUNDA OPCIÓN
+        ElseIf secondChoice Is Nothing
+            pictureB = picture
+            If pictureB.Tag IsNot Nothing Then
+                pictureB.Image = Image.FromFile("cards/" & CStr(pictureB.Tag) & ".png")
+                secondChoice = CStr(pictureB.Tag)
+            End If
+
+            'REFRESCAMOS
+            Application.DoEvents()
+
+            'LLAMAMOS LA FUNCIÓN QUE COMPRUEBA LAS CARTAS
+            CheckPictures(pictureA, pictureB)
+        End If
+
+    End Sub
+
+    Private Sub Shuffle()
+        'DESORDENAMOS EL ARRAY DE NÚMEROS
+        Dim randomList = numbers.OrderBy(Function(x) Guid.NewGuid()).ToList
+        numbers = randomList
+
+        'DAMOS TAG Y ASIGNAMOS IMAGEN
+        For i = 0 To pictures.Count - 1
+            pictures(i).Tag = numbers(i)
+            pictures(i).Image = Image.FromFile("cards/" & pictures(i).Tag & ".png")
+        Next
+
+    End Sub
+
+    Private Sub RestartGame()
+        'BARAJAMOS
+        Shuffle()
+
+        'REINICIAMOS
+        LblLevel.Text = 1
+        LblTime.Text = countDown
+        LblScore.Text = 0
+        Timer1.Start()
+        countDown = totalTime
+
+        checkGameOver = False
+    End Sub
+
+    Private Sub BackCards()
+        'OCULTAMOS CARTAS
+        For i = 0 To pictures.Count - 1
+            pictures(i).Image = Image.FromFile("cards/back.png")
+        Next
+    End Sub
+
+    Private Sub CheckPictures(pictureA As PictureBox, pictureB As PictureBox)
+        'COMPARAMOS LAS DOS ELECCIONES
+        If firstChoice = secondChoice Then
+            pictureA.Tag = Nothing
+            pictureB.Tag = Nothing
+        Else
+            'PENALIZACIÓN
+            countDown -= 2
+            tries += 1
+            LblFail.Text = tries
+
+            'MUESTRA LAS DOS OPCIONES ERRONEAS 1 SEGUNDO
+            Thread.Sleep(750)
+            pictureA.Image = Image.FromFile("cards/back.png")
+            pictureB.Image = Image.FromFile("cards/back.png")
+        End If
+
+        'LIMPIAMOS LAS OPCIONES
+        firstChoice = Nothing
+        secondChoice = Nothing
+
+        'COMPRUEBA SI HA ACERTADO TODAS LAS PAREJAS
+        If pictures.All(Function(o) o.Tag = pictures(0).Tag) Then
+            'MUCHO CUIDADO CON ESTA CONDICION
+            If checkGameOver = False Then
+                YouWin()
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub YouWin()
+        Timer1.Stop()
+        MessageBox.Show("¡Perfecto!", "YouWin")
+
+        'AUMENTO DE LVL
+        numLevel += 1
+        LblLevel.Text = numLevel
+
+        'SCORE
+        LblScore.Text += GetScore(tries)
+
+        'FALLOS
+        tries = 0
+        LblFail.Text = tries
+
+        'VUELVE A EMPEZAR
+        CheckLevel(numLevel)
+        LoadPictures()
+
+    End Sub
+
+    Private Function CheckLevel(numLevel As Integer) As Integer
+        'LIMPIO LAS LIST PARA GENERARLAS NUEVAMENTE
+        If numbers.Count > 0 Then
+            'Borro
+            For index = 0 To numbers.Count
+                numbers(index).remove
+            Next
+            numbers.Clear()
+            pictures.Clear()
+
+            pictures = Nothing
+
+        End If
+
+        Application.DoEvents()
+
+
+        'SUBE LA CANTIDAD DE CARTAS A 4 CADA 5 LVL HASTA UN MÁXIMO DE 20
+        If numLevel >= 1 And numLevel < 5 Then
+            qtyCards = 4
+        Else
+            qtyCards = 6
+        End If
+        Return qtyCards
+    End Function
+
+    Private Function GetScore(tries As Integer) As Integer
+        If tries = 0 Then
+            score = 100
+        Else
+            score = 50
+        End If
+        Return score
+    End Function
+
+    Private Sub GameOver(msg As String)
+        'GAMEOVER
+        Timer1.Stop()
+        checkGameOver = True
+        MessageBox.Show(msg + "Pulse reiniciar para volver a jugar", "¿Seguimos?")
+    End Sub
+
+    Private Sub AcercaDeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AcercaDeToolStripMenuItem.Click
+        'ACERCA DE
+    End Sub
+
+End Class
